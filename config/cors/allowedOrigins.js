d0f3d9c6f5bd8e74327ab7819d1e63d6cfa60c9b
Lemon San <santuele.cl>
2024-01-09 22:17:53 +0800

    change cors

--- config/cors/allowedOrigins.js
+++ config/cors/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://playful-empanada-24d002.netlify.app/",
+];
 
 export default allowedOrigins;
